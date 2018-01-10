9b043a7fda7d6a5fc1f8ebb88c235a4a9301a41c
NickGreen <nick@prospress.com>
2018-01-10 13:11:01 -0800

    Renamed plugin. Using _is_purchasable now instead of _is_visible. Runs without errors.

--- README.md
+++ README.md
@@ -1,16 +1,18 @@
-# WooCommerce Subscriptions One-to-One
+# WooCommerce Subscriptions Restrict Product
 
-Hides subscription products from the store that are currently subscribed to.
+Restricts subscription products to a certain number of total active (unended) subscriptions on a site.
 
 Use case: store owner wants products and subscriptions to have a one-to-one relationship, i.e. each product should only be subscribed to once. For example, subscription product is a child to be sponsored, and only one subscriber should be sponsoring that child at a time.
 
-This plugin creates and maintains a list of products that have unended subscriptions associated with them, then hides these products from the store to make sure that only one person can subscribe to a specific product at a time. It doesn't change ability to check out with product, so that customer can pay for failed order through cart.
+This plugin creates and maintains a list of products that have unended subscriptions associated with them, then makes these products unpurchasable from front end to make sure that the subscription product can only be subscribed to a certain number of times. It doesn't change ability to check out with product, so that customer can pay for failed order through cart, and manual renewals are unaffected.
+
+Note: This plugin is designed to work independently of the WooCommerce inventory system, and so inventory should be disabled for all subscription products, or you may have problems with customers not being able to pay for failed renewal orders manually.
 
 ## Installation
 
 To install:
 
-1. Download the latest version of the plugin [here](https://github.com/Prospress/woocommerce-subscriptions-one-to-one/archive/master.zip)
+1. Download the latest version of the plugin [here](https://github.com/Prospress/woocommerce-subscriptions-restrict-product/archive/master.zip)
 1. Go to **Plugins > Add New > Upload** administration screen on your WordPress site
 1. Select the ZIP file you just downloaded
 1. Click **Install Now**
@@ -22,7 +24,7 @@ To keep the plugin up-to-date, use the [GitHub Updater](https://github.com/afrag
 
 ## Reporting Issues
 
-If you find a problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/woocommerce-subscriptions-one-to-one/issues/new).
+If you find a problem or would like to request this plugin be extended, please [open a new Issue](https://github.com/Prospress/woocommerce-subscriptions-restrict-product/issues/new).
 
 ---
 
--- wcs-restrict-product.php
+++ wcs-restrict-product.php
@@ -1,8 +1,8 @@
 <?php
 /*
-* Plugin Name: WooCommerce Subscriptions One-to-One
-* Plugin URI: https://github.com/Prospress/woocommerce-subscriptions-one-to-one/
-* Description: Hides subscription products from the store that are currently subscribed to.
+* Plugin Name: WooCommerce Subscriptions Restrict Product
+* Plugin URI: https://github.com/Prospress/woocommerce-subscriptions-restrict-product/
+* Description: Restricts subscription products to a certain number of total active (unended) subscriptions on a site.
 * Author: Prospress Inc.
 * Author URI: https://prospress.com/
 * License: GPLv3
@@ -45,15 +45,15 @@ if ( false === PP_Dependencies::is_subscriptions_active( '2.1' ) ) {
 	return;
 }
 
-register_activation_hook( __FILE__,'create_one_to_one_cache'); // creates array of product IDs in the options table when plugin is activated
-add_filter( 'woocommerce_subscription_status_updated', 'update_one_to_one_cache', 10, 2 ); // updates the array whenever subscription status is updated
-add_filter( 'woocommerce_product_is_visible', 'one_to_one_checker', 10, 2 ); // when displaying product on front end, hides product if is in the array
-register_deactivation_hook( __FILE__, 'cleanup_one_to_one_cache' ); // deletes array when plugin is deactivated
+register_activation_hook( __FILE__,'create_wcs_restriction_cache'); // creates array of product IDs in the options table when plugin is activated
+add_filter( 'woocommerce_subscription_status_updated', 'update_wcs_restriction_cache', 10, 3 ); // updates the array whenever subscription status is updated
+add_filter( 'woocommerce_is_purchasable', 'wcs_restriction_is_purchasable', 10, 2 ); // disallows purchasing from the single product page only
+register_deactivation_hook( __FILE__, 'cleanup_wcs_restriction_cache' ); // deletes array when plugin is deactivated
 
 /**
 * creates array of product IDs in the options table when plugin is activated
 */
-function create_one_to_one_cache() {
+function create_wcs_restriction_cache() {
 
 	$products_with_subscription = [];
 
@@ -64,13 +64,11 @@ function create_one_to_one_cache() {
 	foreach ($unended_subscriptions as $subscription) {
 		foreach ( $subscription->get_items() as $item_id => $line_item ) {
 			$product_id = $line_item->get_product_id();
-			if (!in_array($product_id, $products_with_subscription)) {
-				$products_with_subscription[] = $product_id;
-			}
+			$products_with_subscription[$product_id]++;
 		}
 	}
 
-	update_option( 'wcs_one_to_one_cache', $products_with_subscription );
+	update_option( 'wcs_restriction_cache', $products_with_subscription );
 }
 
 /**
@@ -80,55 +78,73 @@ function create_one_to_one_cache() {
 * @param string
 * @param string
 */
-function update_one_to_one_cache($subscription, $new_status, $old_status) {
-
-	$cache = get_option('wcs_unended_subs_cache');
+function update_wcs_restriction_cache($subscription, $new_status, $old_status) {
+error_log("updating cache");
+	$cache = get_option('wcs_restriction_cache');
 
 	$unended_statuses = array('active', 'on-hold', 'pending-cancel');
 
 	if (in_array($new_status, $unended_statuses) && !in_array($old_status, $unended_statuses)) {
 		foreach ( $subscription->get_items() as $item_id => $line_item ) {
 			$product_id = $line_item->get_product_id();
-			// add product id to cache
+			// increment product id in cache
 			if (!in_array($product_id, $cache)) {
-				$cache[] = $product_id;
+				$cache[$product_id]++;
+				error_log("added product " . $product_id);
 			}
 		}
-		update_option( 'wcs_one_to_one_cache', $cache );
+		update_option( 'wcs_restriction_cache', $cache );
 
 	} elseif (in_array($old_status, $unended_statuses) && !in_array($new_status, $unended_statuses)) {
 		foreach ( $subscription->get_items() as $item_id => $line_item ) {
 			$product_id = $line_item->get_product_id();
-			// remove product id from cache
-			$cache	= array_diff($cache, array($product_id));
+
+			if ($cache[$product_id] <= 1) {
+				unset($cache[$product_id]); // remove product from array completely
+				error_log("deleted product " . $product_id);
+			} elseif ($cache[$product_id] > 1) {
+				$cache[$product_id]--; // or just decrement its value
+				error_log("decremented product " . $product_id);
+			}
+
 		}
-		update_option( 'wcs_one_to_one_cache', $cache );
+		update_option( 'wcs_restriction_cache', $cache );
 	}
 
 }
 
 /**
-* when displaying product on front end, hides product if is in the array
+* disallows purchasing from the single product page only
 *
 * @param boolean
+* @param instance of WC_Product object
+*/
+function wcs_restriction_is_purchasable( $purchasable, $product ){
+    if( is_woocommerce() && wcs_is_restricted( $product->get_id() ) ) // cart and checkout are standard pages with shortcodes and thus are not included in the is_woocommerce check. this allows the product to run through checkout for failed renewal payments
+        $purchasable = false;
+    return $purchasable;
+}
+
+/**
+* checks against cache to see if subscription is restricted
+*
 * @param integer
 */
-function one_to_one_checker( $is_visible, $id ) {
-
-	$cache = get_option('wcs_unended_subs_cache');
+function wcs_is_restricted( $id ) {
+	$is_restricted = false;
+	$cache = get_option('wcs_restriction_cache');
+	error_log(print_r($cache, TRUE));
 	if ($cache != false) {
-		$product = wc_get_product( $id );
-
-		if ( in_array($product->get_id(), $cache) ) {
-			$is_visible = false;
+		if ( isset($cache[$id]) && ($cache[$id] >= 1) ) {
+			$is_restricted = true;
 		}
 	}
-	return $is_visible;
+	return $is_restricted;
 }
 
 /**
 * deletes array when plugin is deactivated
 */
-function cleanup_one_to_one_cache() {
-	delete_option( 'wcs_one_to_one_cache' );
+function cleanup_wcs_restriction_cache() {
+	delete_option( 'wcs_restriction_cache' );
 }
