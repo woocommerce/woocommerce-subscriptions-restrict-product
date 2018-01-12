546c1ea56e66494dd26aa3eafb3d3e3d00e5a49f
NickGreen <nick@prospress.com>
2018-01-11 16:53:52 -0800

    Added UI & option for restriction by more than one

--- wcs-restrict-product.php
+++ wcs-restrict-product.php
@@ -46,6 +46,8 @@ if ( false === PP_Dependencies::is_subscriptions_active( '2.1' ) ) {
 }
 
 register_activation_hook( __FILE__,'create_wcs_restriction_cache'); // creates array of product IDs in the options table when plugin is activated
+add_action( 'woocommerce_product_options_advanced', 'wcs_restriction_admin_edit_product_fields' ); //Add product restriction option on edit product page
+add_action( 'woocommerce_process_product_meta', 'wcs_restriction_save_product_fields' ); // Save the data value from the custom fields
 add_filter( 'woocommerce_subscription_status_updated', 'update_wcs_restriction_cache', 10, 3 ); // updates the array whenever subscription status is updated
 add_filter( 'woocommerce_is_purchasable', 'wcs_restriction_is_purchasable', 10, 2 ); // disallows purchasing from the single product page only
 register_deactivation_hook( __FILE__, 'cleanup_wcs_restriction_cache' ); // deletes array when plugin is deactivated
@@ -64,7 +66,8 @@ function create_wcs_restriction_cache() {
 	foreach ($unended_subscriptions as $subscription) {
 		foreach ( $subscription->get_items() as $item_id => $line_item ) {
 			$product_id = $line_item->get_product_id();
-			$products_with_subscription[$product_id]++;
+			$quantity = $line_item->get_quantity();
+			$products_with_subscription[$product_id] += $quantity;
 		}
 	}
 
@@ -87,10 +90,9 @@ error_log("updating cache");
 	if (in_array($new_status, $unended_statuses) && !in_array($old_status, $unended_statuses)) {
 		foreach ( $subscription->get_items() as $item_id => $line_item ) {
 			$product_id = $line_item->get_product_id();
-			// increment product id in cache
+			$quantity = $line_item->get_quantity();
 			if (!in_array($product_id, $cache)) {
-				$cache[$product_id]++;
-				error_log("added product " . $product_id);
+				$cache[$product_id] += $quantity;
 			}
 		}
 		update_option( 'wcs_restriction_cache', $cache );
@@ -98,12 +100,12 @@ error_log("updating cache");
 	} elseif (in_array($old_status, $unended_statuses) && !in_array($new_status, $unended_statuses)) {
 		foreach ( $subscription->get_items() as $item_id => $line_item ) {
 			$product_id = $line_item->get_product_id();
-
+			$quantity = $line_item->get_quantity();
 			if ($cache[$product_id] <= 1) {
 				unset($cache[$product_id]); // remove product from array completely
 				error_log("deleted product " . $product_id);
 			} elseif ($cache[$product_id] > 1) {
-				$cache[$product_id]--; // or just decrement its value
+				$cache[$product_id] -= $quantity; // or just reduce the number
 				error_log("decremented product " . $product_id);
 			}
 
@@ -111,6 +113,40 @@ error_log("updating cache");
 		update_option( 'wcs_restriction_cache', $cache );
 	}
 
+}
+
+/**
+ * Adds restrict product option to 'Edit Product' screen.
+ */
+function wcs_restriction_admin_edit_product_fields() {
+	global $post;
+
+	echo '<div class="options_group restrict_product show_if_subscription show_if_variable-subscription">';
+
+	woocommerce_wp_text_input( array(
+		'id'          => '_product_restriction',
+		'label'       => __( 'Restrict product', 'woocommerce-subscriptions' ),
+		'desc_tip' => 'true',
+		'description' => __( 'Restrict product to a total number of subscriptions on your site. Leave blank or set to "0" to deactivate.' ),
+		'placeholder' => '0',
+		'type' => 'number',
+		'custom_attributes' => array(
+			'step' => '1',
+			'min' => '0', )
+	) );
+	echo '</div>';
+
+	do_action( 'woocommerce_subscriptions_product_options_advanced' );
+}
+
+/**
+* Save the data value from the custom field
+*
+* @param int
+*/
+function wcs_restriction_save_product_fields( $post_id ) {
+	$product_restriction = $_POST['_product_restriction'];
+	update_post_meta( $post_id, '_product_restriction', esc_attr( $product_restriction ) );
 }
 
 /**
@@ -126,16 +162,18 @@ function wcs_restriction_is_purchasable( $purchasable, $product ){
 }
 
 /**
-* checks against cache to see if subscription is restricted
+* helper function that checks against cache to see if subscription is restricted
 *
 * @param integer
 */
 function wcs_is_restricted( $id ) {
 	$is_restricted = false;
 	$cache = get_option('wcs_restriction_cache');
-	error_log(print_r($cache, TRUE));
+	$product_restriction_option = get_post_meta( $id, '_product_restriction', true );
+	if ( !isset($product_restriction_option) || empty($product_restriction_option)) $product_restriction_option = 0;
+	// error_log(print_r($cache, TRUE));
 	if ($cache != false) {
-		if ( isset($cache[$id]) && ($cache[$id] >= 1) ) {
+		if ( isset($cache[$id]) && ($product_restriction_option > 0) && ($cache[$id] >= $product_restriction_option) ) {
 			$is_restricted = true;
 		}
 	}
